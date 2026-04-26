45d0e6f86a2122d84e0ea75ded24a1596436e185
Srinath K K <srinathkk99@gmail.com>
2026-04-26 11:20:40 +0530

    connected backend

--- src/utils/ai.js
+++ src/utils/ai.js
@@ -1,5 +1,11 @@
 export async function askAI(question) {
-    const res = await fetch("https://cortexsec-server.onrender.com/ask")
+    const res = await fetch("https://cortexsec-server.onrender.com/ask", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question }),
+    });
     const data = await res.json();
     return data.answer;
 }
